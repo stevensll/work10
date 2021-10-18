c041a53dcbff92db405fec6b17c99bca944bb00d
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-10-18 14:47:42 -0400

    Create linkedlist.h

---
+++ linkedlist.h
@@ -0,0 +1,5 @@
+void print list ( struct node * node);
+struct node * make_node(int id, char *name);
+struct node * insert_front(struct node *f, int id, char * name);
+struct node * free_list (struct node * n);
+struct node * remove_node(struct node *front, int data);
